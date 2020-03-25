8deabfabd67ec2a922e7a5f004684ac0a1131fd7
dalin <dalinyy@163.com>
2020-03-25 23:58:11 +0800

    添加02-图像基本操作

--- 02-图像基本操作/exercise.py
+++ 02-图像基本操作/exercise.py
@@ -10,4 +10,28 @@
 import cv2
 
 img = cv2.imread('img\imgtest.jpg')
-print(img)
+# print(img)
+# px = img[100, 100]
+# print(px)
+# px_blue = img[100, 100, 0]
+# print(px_blue)
+# print(img.shape)
+# height, width, channels = img.shape
+# print(height)
+# print(img.dtype)
+# print(img.size)
+
+# 截取脸部ROI
+# face = img[80:280, 75:230]
+# cv2.imshow('face', face)
+# cv2.waitKey(0)
+
+# 通道分割与合并
+# b, g, r = cv2.split(img)
+# img = cv2.merge((b, g, r))
+# cv2.imshow('img', img)
+# cv2.waitKey(0)
+
+b = img[:, :, 2]
+cv2.imshow('blue', b)
+cv2.waitKey(0)

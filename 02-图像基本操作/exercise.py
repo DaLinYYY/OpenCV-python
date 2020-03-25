5cd017b02311b711bf3eba882e006539b8ed4e77
dalin <dalinyy@163.com>
2020-03-25 23:42:29 +0800

    添加02-图像基本操作

---
+++ 02-图像基本操作/exercise.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   exercise.py 
+@author  :   dalin  
+@Contact :   dalin.strive@gmail.com
+@Date     :  2020/3/25 
+"""
+
+import cv2
+
+img = cv2.imread('img\imgtest.jpg')
+print(img)
